7a79c8b2cd259ee6a07fe77368b71e0977de1f87
mt👾💻📈 <benimadidi100@gmail.com>
2025-05-25 00:09:42 +0100

    Petite modification au niveau du formatage de la docu

--- MySQL_cours/fonctions_d_agregat.sql
+++ MySQL_cours/fonctions_d_agregat.sql
@@ -1,15 +1,15 @@
 
 /*
-    Les fonctions d'agregat sont des fonctions qui permettent de faire des calculs sur des groupes de données.
-    Parmi ces fonctions, il y a :
-        - COUNT() : permet de compter le nombre de lignes d'une table
-            Ex : SELECT COUNT(*) FROM <nom_table>
-        - AVG : permet de calculer la moyenne des valeurs d'une colonne
-            Ex : SELECT AVG(<nom_colonne>) FROM <nom_table>
-        - MAX : permet de trouver la valeur maximale d'une colonne
-            Ex : SELECT MAX(<nom_colonne>) FROM <nom_table>
-        - MIN : permet de trouver la valeur minimale d'une colonne
-            Ex : SELECT MIN(<nom_colonne>) FROM <nom_table>
+    - Les fonctions d'agregat sont des fonctions qui permettent de faire des calculs sur des groupes de données.
+        Parmi ces fonctions, il y a :
+            - COUNT() : permet de compter le nombre de lignes d'une table
+                Ex : SELECT COUNT(*) FROM <nom_table>
+            - AVG : permet de calculer la moyenne des valeurs d'une colonne
+                Ex : SELECT AVG(<nom_colonne>) FROM <nom_table>
+            - MAX : permet de trouver la valeur maximale d'une colonne
+                Ex : SELECT MAX(<nom_colonne>) FROM <nom_table>
+            - MIN : permet de trouver la valeur minimale d'une colonne
+                Ex : SELECT MIN(<nom_colonne>) FROM <nom_table>
         
 */
 
