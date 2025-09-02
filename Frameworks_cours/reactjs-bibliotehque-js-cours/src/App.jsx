53ed27d90c220d763aa07c61844b69c206bed129
mt👾💻📈 <benimadidi100@gmail.com>
2025-09-02 18:24:42 +0100

    Preparation pour le premier exercice en react

--- Frameworks_cours/reactjs-bibliotehque-js-cours/src/App.jsx
+++ Frameworks_cours/reactjs-bibliotehque-js-cours/src/App.jsx
@@ -3,8 +3,12 @@ import LeHookUseState from './cours/le_hook_useState';
 import LesFormulaires from './cours/les_formulaires';
 import LefluxDeDonnees from './cours/le_flux_de_donnees';
 
+//Exercice
+import  ListeDeProduit from "C:/Users/hp/Desktop/Vs_code/Exercices-prog/exercices-frameworks/ReactJS/liste_de_produit/index"
+
+
 function App() {
-  return <LefluxDeDonnees />;
+  return <ListeDeProduit />;
 }
 
 export default App
