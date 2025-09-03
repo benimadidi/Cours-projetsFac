8df46329f63e35df4b2718aa56eeea6f7fb613fe
mt👾💻📈 <benimadidi100@gmail.com>
2025-09-03 18:10:10 +0100

    Ajout de la notion sur le hookUseEffect

--- Frameworks_cours/reactjs-bibliotehque-js-cours/src/App.jsx
+++ Frameworks_cours/reactjs-bibliotehque-js-cours/src/App.jsx
@@ -2,13 +2,14 @@ import LaSyntaxeJsx from './cours/la_syntaxe_jsx';
 import LeHookUseState from './cours/le_hook_useState';
 import LesFormulaires from './cours/les_formulaires';
 import LefluxDeDonnees from './cours/le_flux_de_donnees';
+import LeHookUseEffect from './cours/le_hook_useEffect';
 
 //Exercice
 import  ListeDeProduit from "C:/Users/hp/Desktop/Vs_code/Exercices-prog/exercices-frameworks/ReactJS/liste_de_produit/index"
 
 
 function App() {
-  return <ListeDeProduit />;
+  return <LeHookUseEffect />;
 }
 
 export default App
